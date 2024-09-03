d70e50c6d213f1128801a84cce12a1d3a0e55b32
AgataQuadros <quadrosagata@gmail.com>
2024-09-02 21:31:48 -0300

    Exercício A

--- Atividades/Atividade1/ex_a.py
+++ Atividades/Atividade1/ex_a.py
@@ -2,8 +2,8 @@
 # Turma 0152
 # Autor: Agata Quadros (Gaia)
 # Data 02/09/2024
-# Objetivo: Faça um programa que peça 3 valores , 
-# depois calcule e imprima  a soma e a multiplicação desses valores. 
+# Objetivo: Faça um programa que peça 3 valores ,
+# depois calcule e imprima  a soma e a multiplicação desses valores.
 
 # Biblioteca
 import os
@@ -14,3 +14,37 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO A')
 print('-' * 20)
+
+
+# Criando a Classe Soma
+class Calculos:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    # Metodo Somar
+    def somar(self):
+        resultado = self.a + self.b + self.c
+        return resultado
+    
+    # Metodo Multiplicar
+    def multiplicar(self):
+        resultado = self.a * self.b * self.c
+        return resultado
+
+# Entrada
+os.system('cls')
+print('-' * 20)
+a = int(input('Entre com o 1º valor: '))
+b = int(input('Entre com o 2º valor: '))
+c = int(input('Entre com o 3º valor: '))
+
+# Objeto da classe
+valores = Calculos(a, b, c)
+
+# Saida
+print('-' * 20)
+print(f'A soma dos valores propostos é: {valores.somar()}')
+print(f'O produto dos valores propostos é: {valores.multiplicar()}')
+print('=' * 50)
