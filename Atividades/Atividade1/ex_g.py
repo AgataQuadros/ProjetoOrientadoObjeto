a0c832f464d85163a33c9576d914a69702df5ecd
AgataQuadros <quadrosagata@gmail.com>
2024-09-04 20:20:49 -0300

    Exercício G

--- Atividades/Atividade1/ex_g.py
+++ Atividades/Atividade1/ex_g.py
@@ -14,3 +14,32 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO G')
 print('-' * 20)
+
+# Iniciando Classe
+class Converter:
+    def __init__(self, metros):
+        self.metros = metros
+    
+    def centimetos(self):
+        centimetros = metros * 100
+        return centimetros
+    
+    def milimetros(self):
+        milimetros = metros * 1000
+        return milimetros
+
+# Entrada
+metros = float(input('Digite o metro: '))
+
+resultado = Converter(metros)
+centimetros = resultado.centimetos()
+milimetros = resultado.milimetros()
+
+# Saida
+print('')
+print('-' * 20)
+print(f'A converção dos metros {metros} para centímetros é {centimetros} cm')
+print(f'A converção dos metros {metros} para milímetros é {milimetros} mm')
+print('-' * 20)
+print('')
+print('=' * 50)
