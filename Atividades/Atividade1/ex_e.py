5e49567d85600c336ac878596aa58cde38f81a0c
AgataQuadros <quadrosagata@gmail.com>
2024-09-04 19:42:08 -0300

    Exercício E

--- Atividades/Atividade1/ex_e.py
+++ Atividades/Atividade1/ex_e.py
@@ -14,3 +14,31 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO E')
 print('-' * 20)
+
+# Iniciando a classe
+class Ante_Sucess:
+    def __init__(self, numero):
+        self.numero = numero
+    
+    def antecessor(self):
+        antecessor = self.numero - 1
+        return antecessor
+
+    def sucessor(self):
+        sucessor = self.numero + 1
+        return sucessor
+
+# Entrada
+numero = int(input('Entre com um numero inteiro: '))
+resultado = Ante_Sucess(numero)
+antecessor = resultado.antecessor()
+sucessor = resultado.sucessor()
+
+# Saida
+print('')
+print('-' * 20)
+print(f'O sucessor do numero {numero} é {sucessor}')
+print(f'O antecessor do numero {numero} é {antecessor}')
+print('-' * 20)
+print('')
+print('=' * 50)
