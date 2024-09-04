51849d612a6d01c712f97d41db8b152dc286ddb9
AgataQuadros <quadrosagata@gmail.com>
2024-09-04 19:50:40 -0300

    Exercício F

--- Atividades/Atividade1/ex_f.py
+++ Atividades/Atividade1/ex_f.py
@@ -14,3 +14,33 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO F')
 print('-' * 20)
+
+# Iniciando classe
+class Dub_trip:
+    def __init__(self, numero):
+        self.numero = numero
+    
+    def triplo(self):
+        triplo = numero * 3
+        return triplo
+    
+    def dobro(self):
+        dobro = numero * 2
+        return dobro
+
+
+
+# Entrada
+numero = (float(input('Digite o numero: ')))
+
+resultado = Dub_trip(numero)
+dobro = resultado.dobro()
+triplo = resultado.triplo()
+
+# Saída
+print('-' * 20)
+print('')
+print(f'O drobro de {numero} é {dobro}')
+print(f'enquanto seu tríplo é {triplo}')
+print('')
+print('=' * 50)
