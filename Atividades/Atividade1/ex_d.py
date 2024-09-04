24360394ef2320a06cb756e6193c5698d4526487
AgataQuadros <quadrosagata@gmail.com>
2024-09-03 21:35:14 -0300

    Exercício D

--- Atividades/Atividade1/ex_d.py
+++ Atividades/Atividade1/ex_d.py
@@ -14,3 +14,22 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO D')
 print('-' * 20)
+
+# Criando classe
+class Divisao:
+    def __init__(self, ):
+        print
+# Entrada
+dividendo = (float(input('Entre com o divendendo: ')))
+divisor = (float(input('Entre com o divisor: ')))
+
+# Processamento
+resultado = dividendo / divisor
+
+# Saída
+print('')
+print('-' * 20)
+print(f'O resultado da divisão entre {dividendo} / {divisor} é {resultado: .4f}')
+print('-' * 20)
+print('')
+print('=' * 50)
