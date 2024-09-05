2784316dd443f625886cfccc09400e790292b971
AgataQuadros <quadrosagata@gmail.com>
2024-09-04 21:24:58 -0300

    Exercício J

--- Atividades/Atividade1/ex_j.py
+++ Atividades/Atividade1/ex_j.py
@@ -14,3 +14,29 @@ os.system('cls')
 print('=' * 50)
 print('EXERCÍCIO J')
 print('-' * 20)
+
+# Criando classe
+class Retangulo:
+    def __init__(self, lado1, lado2):
+        self.lado1 = lado1
+        self.lado2 = lado2
+    
+    def calcular(self):
+        resolver = (lado1 * 2) + (lado2 * 2)
+        return resolver
+
+
+# Etrada
+lado1 = (float(input('Entre com o lado 1: ')))
+lado2 = (float(input('Entre com o lado 2: ')))
+
+conta = Retangulo(lado1, lado2)
+resultado = conta.calcular()
+
+# Saída
+print('')
+print('-' * 20)
+print(f'O perímetro do retangulo é {resultado}')
+print('-' * 20)
+print('')
+print('=' * 50)
