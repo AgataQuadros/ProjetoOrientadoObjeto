1351b951ea7f9e82e8fbf0f286d7b75d61487085
AgataQuadros <quadrosagata@gmail.com>
2024-09-09 19:49:01 -0300

    Exercício D

--- Atividades/Atividade2/c.py
+++ Atividades/Atividade2/c.py
@@ -16,12 +16,36 @@ print('=' * 50)
 print('EXERCÍCIO C')
 print('-' * 20)
 
-# Processamento
-for c in range(10, 0, -1):
-    print('----')
-    print(f'{c}')
+
+# Criando as classes
+class Numeros:
+    def __init__(self, inicio, meio, fim):
+        self.inicio = inicio
+        self.meio = meio
+        self.fim = fim
+    
+    def imprimir_sequencia(self, inicio, meio, fim):
+        # este metodo vai ser sobrecarregado
+        print()
+
+
+class Sequencia(Numeros):
+    def __init__(self, inicio, meio, fim):
+        self.inicio = inicio
+        self.meio = meio
+        self.fim = fim
+    
+    def imprimir_sequencia(self):
+        for numero in range(self.inicio, self.meio, self.fim):
+            print(numero, end=' ')
+
+
+# Processamento]
+sequencia_numeros = Sequencia(10, 0, -1)
+sequencia_numeros.imprimir_sequencia()
 
 # Saida
+print()
 print('-' * 20)
 print('Fim do exercício :D')
 print('=' * 50)
