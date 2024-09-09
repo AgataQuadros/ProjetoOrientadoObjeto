5d409c79d402844dcfe155d571461656b215d4a3
AgataQuadros <quadrosagata@gmail.com>
2024-09-09 20:00:16 -0300

    Exercício D

--- Atividades/Atividade2/d.py
+++ Atividades/Atividade2/d.py
@@ -15,13 +15,35 @@ print('=' * 50)
 print('EXERCÍCIO D')
 print('-' * 20)
 
-for c in range(0,100):
-    if c % 2 == 0:
-        print('----')
-        print(c)
 
+# Criando as classes
+class Sequencia:
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+    
+    def sequencia_pares(self, inicio, fim):
+        # este metodo vai ser sobrecarregado
+        print()
+
+
+class Pares(Sequencia):
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+    
+    def sequencia_pares(self):
+        for numero in range(self.inicio, self.fim):
+            if numero % 2 == 0:
+                print(numero, end=' ')
+
+
+# Processamento
+numeros_pares = Pares(0, 100)
+numeros_pares.sequencia_pares()
  
 # Saida
+print()
 print('-' * 20)
 print('Fim do exercício :D')
 print('=' * 50)
