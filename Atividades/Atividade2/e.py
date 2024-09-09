c82b99255e516e130f32a1d7a73b5f370677cd13
AgataQuadros <quadrosagata@gmail.com>
2024-09-09 20:39:25 -0300

    BBBBBBBBBBBBBBBBB

--- Atividades/Atividade2/e.py
+++ Atividades/Atividade2/e.py
@@ -16,6 +16,39 @@ print('=' * 50)
 print('EXERCÍCIO E')
 print('-' * 20)
 
+
+# Criando as Classes
+class Numeros:
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+
+    def quantidade_numeros(self, inicio, fim):
+        # este metodo vai ser sobrecarregado
+        pass
+
+    def soma_numeros(self, inicio, fim):
+        # este metodo vai ser sobrecarregado
+        pass
+
+
+class Sequencia(Numeros):
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+    
+    def quantidade_numeros(self):
+        for numero in range(self.inicio, self.fim):
+            if numero % 2 == 0:
+
+    def soma_numeros(self):
+        # este metodo vai ser sobrecarregado
+        pass
+
+
+
+
+
 # Entrada
 quantidade_pares = 0
 soma = 0
