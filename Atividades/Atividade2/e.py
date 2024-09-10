2fe5fa373728109715f13dd336c6f87fdb451c37
AgataQuadros <quadrosagata@gmail.com>
2024-09-09 21:06:24 -0300

    Exercício E

--- Atividades/Atividade2/e.py
+++ Atividades/Atividade2/e.py
@@ -2,7 +2,7 @@
 # Turma 0152
 # Autor: Agata Quadros (Gaia)
 # Data 06/09/2024
-# Objetivo: Faça um programa que some a quantidade 
+# Objetivo: Faça um programa que some a quantidade
 # de números pares encontrados no intervalo entre 0 e 100.
 
 # Biblioteca
@@ -27,42 +27,28 @@ class Numeros:
         # este metodo vai ser sobrecarregado
         pass
 
-    def soma_numeros(self, inicio, fim):
-        # este metodo vai ser sobrecarregado
-        pass
-
 
 class Sequencia(Numeros):
     def __init__(self, inicio, fim):
         self.inicio = inicio
         self.fim = fim
-    
+
     def quantidade_numeros(self):
+        quantidade_pares = 0
+        somatorio = 0
+
         for numero in range(self.inicio, self.fim):
             if numero % 2 == 0:
-
-    def soma_numeros(self):
-        # este metodo vai ser sobrecarregado
-        pass
-
-
-
-
-
-# Entrada
-quantidade_pares = 0
-soma = 0
+                quantidade_pares += 1
+                somatorio += numero
+        print(quantidade_pares)
+        print(somatorio)
 
 
 # Processamento
-for c in range(0, 102):
-    if c % 2 ==0:
-        quantidade_pares += 1
-        soma += c
-        
-print('----')
-print(quantidade_pares)
-print(soma)
+intervalo = Sequencia(0, 101)
+intervalo.quantidade_numeros()
+
 
 # Saida
 print('-' * 20)
