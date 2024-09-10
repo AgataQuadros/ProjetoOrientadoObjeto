130814d357fb6136f99e8394ff7630611319930d
AgataQuadros <quadrosagata@gmail.com>
2024-09-10 19:50:59 -0300

    Exercício H

--- Atividades/Atividade2/h.py
+++ Atividades/Atividade2/h.py
@@ -18,6 +18,36 @@ print('=' * 50)
 print('EXERCÍCIO H')
 print('-' * 20)
 
+
+# Criando classes
+class Numeros:
+    def __init__(self, inicio, fim, exclui1, exclui2, exclui3):
+        self.inicio = inicio
+        self.fim = fim
+        self.exclui1 = exclui1
+        self.exclui2 = exclui2
+        self.exclui3 = exclui3
+
+    def excluir_nemeros(self, inicio, fim,  exclui1, exclui2, exclui3):
+        # este metodo vai ser sobrecarregado
+        pass
+
+
+class Excluindo(Numeros):
+    def __init__(self, inicio, fim, exclui1, exclui2, exclui3):
+        self.inicio = inicio
+        self.fim = fim
+        self.exclui1 = exclui1
+        self.exclui2 = exclui2
+        self.exclui3 = exclui3
+
+    def excluir_nemeros(self):
+        for numero in range((inicio),(fim+1)):
+            if numero == exclui1 or numero == exclui2 or numero == exclui3:
+                continue
+            print(numero, end=" ")
+
+
 # etrada
 inicio = int(input('Entre com o inicio do intervalo: '))
 fim = int(input('Entre com o final do intervalo: '))
@@ -26,13 +56,10 @@ exclui1 = int(input('Entre com o 1º número: '))
 exclui2 = int(input('Entre com o 2º número: '))
 exclui3 = int(input('Entre com o 3º número: '))
 
-# Peocessamento
-for c in range((inicio),(fim)):
-    if c == exclui1 or c == exclui2 or c == exclui3:
-        continue
-    print('')
-    print(c)
-    
+# Processamento
+intervalo = Excluindo(inicio, fim, exclui1, exclui2, exclui3)
+intervalo.excluir_nemeros()
+
 # Saida
 print('')
 print('-' * 20)
