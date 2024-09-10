9e8e9b0fd672c6dcf66a2a0faeab9d8741fbc0af
AgataQuadros <quadrosagata@gmail.com>
2024-09-09 21:16:21 -0300

    Exercício F

--- Atividades/Atividade2/f.py
+++ Atividades/Atividade2/f.py
@@ -16,16 +16,38 @@ print('=' * 50)
 print('EXERCÍCIO F')
 print('-' * 20)
 
+
+# Criando classes
+class Numeros:
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+
+    def numeros_primos(self, inicio, fim):
+        # este metodo vai ser sobrecarregado
+        pass
+
+
+class Primos(Numeros):
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+
+    def numeros_primos(self):
+        for numero in range(self.inicio, self.fim):
+            for primo in range(self.inicio, numero):
+                if numero % primo == 0:
+                    break
+            else:
+                print(numero, end=' ')
+
+
 # Processamento
-for c in range(2,100):
-    for m in range(2,c):
-        if c % m == 0:
-            break
-    else:
-        print('----')
-        print(c)
+intervalo = Primos(2, 100)
+intervalo.numeros_primos()
 
 # Saida
+print()
 print('-' * 20)
 print('Fim do exercício :D')
 print('=' * 50)
