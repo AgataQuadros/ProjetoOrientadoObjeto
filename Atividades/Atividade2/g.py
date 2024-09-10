944493d645d02064486a04f91fc00d23ce49cb8f
AgataQuadros <quadrosagata@gmail.com>
2024-09-09 21:21:40 -0300

    Exercício G

--- Atividades/Atividade2/g.py
+++ Atividades/Atividade2/g.py
@@ -17,17 +17,46 @@ print('=' * 50)
 print('EXERCÍCIO G')
 print('-' * 20)
 
+
+# Criando classes
+class Numeros:
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+
+    def numeros_primos(self, inicio, fim):
+        # este metodo vai ser sobrecarregado
+        pass
+
+
+class Primos(Numeros):
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+
+    def numeros_primos(self):
+         if inicio < 2:
+            print(f'Não existe numero primo de {inicio}, tente outro numero')
+         else:
+            for numero in range(self.inicio, self.fim):
+                  for primo in range(self.inicio, numero):
+                     if numero % primo == 0:
+                        break
+                  else:
+                     print(numero, end=' ')
+
+
+
 # Entrada
 inicio = int(input('Entre com o inicio do intervalo: '))
 fim = int(input('Entre com o final do intervalo: '))
 
 # Processamento
-if inicio < 2:
-   print(f'Não existe numero primo de {inicio}, tente outro numero')
-else:
-   for c in range((inicio),(fim)):
-      for m in range(inicio,c):
-         if c % m == 0:
-            break
-      else:
-         print(c, end=' - ')
+intervalo = Primos(inicio, fim)
+intervalo.numeros_primos()
+
+# Saida
+print()
+print('-' * 20)
+print('Fim do exercício :D')
+print('=' * 50)
