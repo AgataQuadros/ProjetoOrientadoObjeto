5ca3b652e411d0bc597f332c45f9a2c6f7bae718
AgataQuadros <quadrosagata@gmail.com>
2024-09-11 19:54:46 -0300

    Sobrecarregando Método

---
+++ Aula004_Sebrecargas/sobrecarga_metodo.py
@@ -0,0 +1,28 @@
+# Sobrecarrega de  Metodos
+
+class ClassePai: # Super Classe
+    # Metodo construtor
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    # Para sobrecarregar
+    # Vai ser usada para somar 2 numeros
+    def metodo_classe(self, a, b):
+        pass
+
+
+class Classefilha(ClassePai): # Classe Derivada
+    # Metodo construtos
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    # Sobrecarga de metodo
+    def metodo_classe(self):
+        return self.a + self.b
+    
+
+# Programa principal
+teste = Classefilha(1, 2)
+teste.metodo_classe()
