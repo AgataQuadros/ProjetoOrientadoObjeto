550f29420aa0f9dc7edc7307c60a142d038e2e8a
AgataQuadros <quadrosagata@gmail.com>
2024-09-12 21:22:34 -0300

    Teste

--- teste.py
+++ teste.py
@@ -1,1 +1,16 @@
-# Hello
+class ClassePai:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+class ClasseFilha(ClassePai):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def soma(self):
+        soma = self.a + self.b
+        print(f'A soma é {soma}')
+
+conta = ClasseFilha(1, 2)
+conta.soma()
